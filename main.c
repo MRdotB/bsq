6b6b784e12c1172213903292474e62cfa6406849
mrdotb <baptiste.chaleil@gmail.com>
2015-12-26 17:33:15 +0100

    Main get file info done

--- main.c
+++ main.c
@@ -1,17 +1,41 @@
 #include "header.h"
 
-int		is_valid_map(char* str)
+char	*str_cut_3_last(char *str)
 {
-	if (*str)
-		return (1);
-	return (0);
+	char *r;
+	int length;
+
+	r = malloc(4);
+	length = ft_strlen(str);
+	r[3] = '\0';	
+	r[2] = str[length - 1];
+	r[1] = str[length - 2];
+	r[0] = str[length - 3];
+	str[length - 3] = '\0';
+	return (r);
 }
 
-int		is_number(char c)
+char*	ft_concat(char *str1, char *str2)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+	char *r;
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	r = malloc(ft_strlen(str1) + ft_strlen(str2) + 1);
+	while (str1[i])
+	{
+		r[i] = str1[i];
+		i++;
+	}
+	while (str2[j])
+	{
+		r[i + j] = str2[j];
+		j++;
+	}
+	r[i + j] = '\0';
+	return (r);
 }
 
 void	get_file_info(char* file_name, t_file_info *r)
@@ -19,24 +43,30 @@ void	get_file_info(char* file_name, t_file_info *r)
 	int		ret;
 	int		fd;
 	char	*buf;
+	char	*str;
 
+	str = "";
 	buf = malloc(2);
 	if ((fd = open(file_name, O_RDONLY)) == -1)
+	{
 		ft_putstr_err("map error");
-	while ((ret = read(fd, buf, 1)))
+		return ;
+	}
+	while ((ret = read(fd, buf, 1)) && buf[0] != '\n')
 	{
 		buf[ret] = '\0';
-		ft_putstr(buf);
+		str = ft_concat(str, buf);
 	}
-	r->sign = ".xo";
-	r->nb_line = 10;
-	r->total = 14;
+	r->total = ft_strlen(str) + 1;
+	r->sign = str_cut_3_last(str); 
+	r->nb_line = ft_atoi(str);
 }
 
 int		main(void)
 {
 	t_file_info r;
+
 	get_file_info("file1", &r);
-	printf("%s\n", r.sign);
+	printf("r.total = %d | r.sign = %s | r.nb_line = %d\n", r.total, r.sign, r.nb_line);
 	return (0);
 }
