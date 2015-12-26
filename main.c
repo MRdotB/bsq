a5081052b61763f1a3db56eda2a1b0e0a1a12d56
mrdotb <baptiste.chaleil@gmail.com>
2015-12-26 20:29:04 +0100

    hold 2 cases of error map

--- main.c
+++ main.c
@@ -15,29 +15,6 @@ char	*str_cut_3_last(char *str)
 	return (r);
 }
 
-char*	ft_concat(char *str1, char *str2)
-{
-	char *r;
-	int	i;
-	int j;
-
-	i = 0;
-	j = 0;
-	r = malloc(ft_strlen(str1) + ft_strlen(str2) + 1);
-	while (str1[i])
-	{
-		r[i] = str1[i];
-		i++;
-	}
-	while (str2[j])
-	{
-		r[i + j] = str2[j];
-		j++;
-	}
-	r[i + j] = '\0';
-	return (r);
-}
-
 void	get_file_info(char* file_name, t_file_info *r)
 {
 	int		ret;
@@ -48,10 +25,7 @@ void	get_file_info(char* file_name, t_file_info *r)
 	str = "";
 	buf = malloc(2);
 	if ((fd = open(file_name, O_RDONLY)) == -1)
-	{
-		ft_putstr_err("map error");
-		return ;
-	}
+		return (ft_putstr_err("map error"));
 	while ((ret = read(fd, buf, 1)) && buf[0] != '\n')
 	{
 		buf[ret] = '\0';
@@ -67,11 +41,47 @@ void	get_file_info(char* file_name, t_file_info *r)
 	close(fd);
 }
 
+int		is_char_in_str(char c, char *match)
+{
+	while (*match)
+		if (c == *match++)
+			return (1);
+	return (0);
+}
+
+int		is_map_valid(char *file_name, t_file_info *r)
+{
+	int ret;
+	int fd;
+	int count;
+	char buf[2];
+
+	fd = open(file_name, O_RDONLY);
+	// jump info
+	read(fd, buf, r->info_len);
+	count = 0;
+	while ((ret = read(fd, buf, 1)))
+	{
+		buf[ret] = '\0';
+		if (!(is_char_in_str(buf[0], r->sign) || buf[0] == '\n'))
+			return (0);
+		count++;
+		printf("%d", count);
+		printf("|%s|\n", buf);
+	}
+	if (count != ((r->x_max + 1) * r->y_max))
+		return (0);
+	printf("count = %d | x * y = %d\n", count, ((r->x_max + 1) * r->y_max));
+	close(fd);
+	return (1);
+}
+
 int		main(void)
 {
 	t_file_info r;
 
-	get_file_info("file2", &r);
+	get_file_info("file1", &r);
+	printf("is_map_valid = %d \n", is_map_valid("file1", &r));
 	printf("r.info_len = %d | r.sign = %s | ", r.info_len, r.sign);
 	printf("r.x_max = %d | r.y_max = %d\n", r.x_max, r.y_max);
 	return (0);
