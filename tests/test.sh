eab6e1f9ff26b6263fc1bcebe2de942cd6e83e53
mrdotb <baptiste.chaleil@gmail.com>
2015-12-27 11:31:28 +0100

    write shell tests

--- tests/test.sh
+++ tests/test.sh
@@ -21,14 +21,24 @@ echo "$B""_______________Begin tests_______________""$N"
 # test1 if the file doesn't exist
 ./bsq void_file 2>&1 | cat -e > a
 echo "map error" | cat -e > b
-func_cmp a b
+func_cmp a b "1.If the file doesn't exist"
+
 # test2 if the file is empty
+./bsq empty_file 2>&1 | cat -e > a
+func_cmp a b "2.If the file is empty"
 
 # test3 if the file info is invalid
+./bsq bad_file_info 2>&1 | cat -e > a
+func_cmp a b "3.If the file info is invalid"
 
 # test when file info is valid
 # test4 if the map have an invalid sign
+./bsq invalid_sign 2>&1 | cat -e > a
+func_cmp a b "4.If there is an invalid sign in the map"
+
 # test5 if the map contain more or less than (x + 1) * y
+./bsq 2_much 2>&1 | cat -e > a
+func_cmp a b "5.If the map contain more or less than (x+1) * y"
 
 # test6 if no file is provied it should read on 0
 echo "$B""_______________End tests_______________""$N"
