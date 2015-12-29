8ecb8b56c3bcb2c48892474d43011612c131080a
mrdotb <baptiste.chaleil@gmail.com>
2015-12-29 20:58:31 +0100

    take niko filter x and bapti filter y

--- nucleus.c
+++ nucleus.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "lib.h"
-/*
+
 static int  filtered_x_ex(char *tab, int len, int i)
 {
 	int     tmp;
@@ -46,39 +46,6 @@ char		*filtered_x(char *tab, int len)
 	}
 	return (tab);
 }
-*/
-static char	*empty_to_filled(char *tab)
-{
-	int i;
-
-	i = 0;
-	while (tab[i] != '\n')
-	{
-		if (tab[i] == '.')
-			tab[i] = 'x';
-		i++;
-	}
-	return (tab);
-}
-
-char		*filtered_x(char *tab, int len)
-{
-	int i;
-	int j;
-
-	i = 0;
-	tab = empty_to_filled(tab);
-	while (tab[i] != '\n')
-	{
-		j = 0;
-		while (tab[i] == 'x' && j++ < len && (tab[i - 1] != 'x' || i == 0))
-			if (tab[i + j] != 'x' || tab[i + j] == '\n')
-				while (j--)
-					tab[i + j] = '.';
-		i++;
-	}
-	return (tab);
-}
 
 char		**filtered_y(char **tab, int len, int x, t_file_info *r)
 {
@@ -100,48 +67,3 @@ char		**filtered_y(char **tab, int len, int x, t_file_info *r)
 	}
 	return (tab);
 }
-/*
-static void	filtered_y_ex(char **tab, int len, int i, int y)
-{
-	int		tmp;	//	fonction salle mais fonctionnel
-	int		n;
-
-	tmp = i;
-	n = 0;
-	while (len-- && tab[i + 1])
-	{
-		i++;
-		n++;
-		if (tab[i][y] != 'x')
-		{
-	while (n--)
-		tab[tmp++][y] = '.';
-			return ;
-		}	
-	}
-}
-
-char		**filtered_y(char **tab, int len, int y)
-{
-	int		i;
-	int		k;
-
-	i = 0;
-	k = 0;
-	while (tab[i + 1])	//	ça merde !!! si on enlève le +1
-	{
-		if (k == 0)
-		{
-			if (tab[i][y] != 'x')
-				;
-			else
-				filtered_y_ex(tab, len, i, y);
-			k = 1;
-		}
-		if (tab[i][y] != 'x')
-			k = 0;
-		i++;
-	}
-	return (tab);
-}
-*/
