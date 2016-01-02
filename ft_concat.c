5ba14390c4c23ecc658d93657e43b23f99a60b24
bchaleil <Baptiste.chaleil@gmail.com>
2016-01-02 21:45:02 +0100

    norme ft_concat.c

--- ft_concat.c
+++ ft_concat.c
@@ -6,7 +6,7 @@
 /*   By: glodenos <glodenos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2100/01/01 00:00:00 by glodenos          #+#    #+#             */
-/*   Updated: 2100/01/01 00:00:00 by glodenos         ###   ########.fr       */
+/*   Updated: 2016/01/02 21:42:37 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char		*ft_concat(char *str1, char c)
 {
-	char *r;
-	int	i;
+	char	*r;
+	int		i;
 
 	i = 0;
 	r = malloc(ft_strlen(str1) + 2);
